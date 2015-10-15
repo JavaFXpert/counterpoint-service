a7ec86995e9edf8b6b7ee15802ab6c55a4fd90db
Kenny Bastani <kb@socialmoon.com>
2015-10-14 18:27:08 -0700

    Error handling

--- src/main/java/com/culturedear/counterpoint/CounterpointService.java
+++ src/main/java/com/culturedear/counterpoint/CounterpointService.java
@@ -2,11 +2,15 @@ package com.culturedear.counterpoint;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @SpringBootApplication
 public class CounterpointService {
@@ -25,7 +29,7 @@ class XmlRestController {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_XML_VALUE
     )
-    ScorePartwise write(@RequestBody CounterpointModel counterpointModel) throws Exception {
+    ResponseEntity<Object> write(@RequestBody CounterpointModel counterpointModel) {
 
         // prepare response XML out
         CounterpointGenerator cg = new CounterpointGenerator();
@@ -37,6 +41,16 @@ class XmlRestController {
         CounterpointSolution counterpointSolution = cg.anySpecies(counterpointModel.getScaleMode(),
                 vbs, cf.length, counterpointModel.getCounterpointSpecies());
 
-        return counterpointSolution.toScorePartwise();
+        ScorePartwise scorePartwise = null;
+
+        try {
+            scorePartwise = counterpointSolution.toScorePartwise();
+        } catch (NullPointerException ex) {
+            scorePartwise = null;
+        }
+
+        return Optional.ofNullable(scorePartwise)
+                .map(cm -> new ResponseEntity<>((Object)cm, HttpStatus.OK))
+                .orElse(new ResponseEntity<>("Could not compute the counterpoint.",HttpStatus.INTERNAL_SERVER_ERROR));
     }
 }
