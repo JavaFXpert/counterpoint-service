cacca85b4efa3de1a35b022df15445e922063f4b
JavaFXpert <james@j1w.xyz>
2015-10-27 00:13:03 -0400

    Apache 2 license text added to all source files.  Begin building out help documentation, including the About box.

--- src/main/java/com/culturedear/counterpoint/CounterpointGenerator.java
+++ src/main/java/com/culturedear/counterpoint/CounterpointGenerator.java
@@ -1,10 +1,17 @@
 /*
- * CounterpointGenerator.java
+ * Copyright 2015 the original author or authors.
  *
- * TODO: Modify the returned CounterpointSolution returned from anySpecies() so that the voices, including the
- * cantus firmus, are sorted by their initial notes.  This will ensure that a bass clef is never rendered above
- * a treble clef.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.culturedear.counterpoint;
@@ -14,10 +21,6 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author jamesweaver
- */
 @Component
 public class CounterpointGenerator {
   private CounterpointSolution counterpointSolution;
